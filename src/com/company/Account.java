ea19243c0a62533f5cbde7b4346204e54f0bdb63
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 14:39:24 +0300

    Update Bank.java

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -8,5 +8,13 @@ public class Account {
     private int Balance;
     private boolean isLocked;
 
+    public Account(String name){
+        this.Name = name;
+        this.ID = UUID.randomUUID();
+        this.Balance = 0;
+        this.isLocked = false;
+    }
 
+    public String getName(){return Name;}
+    public int getBalance(){return Balance;}
 }
--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -5,25 +5,40 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Bank {
+    //these are class fields
     private String Name;
     private ArrayList<Account> Accounts;
     private UUID ID;
 
+    //this is class constructor
     public Bank(String name){
         this.Name = name;
         this.Accounts = new ArrayList<Account>();
         this.ID = UUID.randomUUID();
     }
 
+    //these are getters for class fields
     public String getName(){return Name;}
     public ArrayList<Account> getAccounts(){return Accounts;}
     public UUID getID(){return ID;}
 
+    //void for adding an account
     public void addAccount(Account account){
         Accounts.add(account);
     }
 
+    //void to make transfer
     public void Transfer(Account to, Account from, int Sum){
 
+    }
+
+    //void to count total bank balance
+    public int getBankBalance(){
+        int balance = 0;
+        for (Account account:Accounts
+             ) {
+            balance += account.getBalance();
+        }
+        return balance;
     }
 }
