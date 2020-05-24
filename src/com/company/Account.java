7eb83b9c7a8ed457752001335ad21e091d2ef22a
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 14:43:57 +0300

    Update Account.java

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -3,11 +3,13 @@ package com.company;
 import java.util.UUID;
 
 public class Account {
+    //class fields
     private String Name;
     private UUID ID;
     private int Balance;
     private boolean isLocked;
 
+    //class constructor
     public Account(String name){
         this.Name = name;
         this.ID = UUID.randomUUID();
@@ -15,6 +17,25 @@ public class Account {
         this.isLocked = false;
     }
 
+    //these are getters for class fields
     public String getName(){return Name;}
     public int getBalance(){return Balance;}
+
+    //void for making deposit (add money)
+    public boolean makeDeposit(int sum){
+        Balance += sum;
+        return true;
+    }
+
+    //void for making withdrawal
+    public boolean makeWithdrawal(int sum){
+        if (Balance >= sum){
+            Balance -= sum;
+            return true;
+        }
+        else{
+            System.out.println("Not enough money in " + ID + " account!");
+            return false;
+        }
+    }
 }
