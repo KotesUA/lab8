299dedeefb98aa0db259f16bc9a87626dd199f6c
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 15:06:22 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Bank bank = new Bank("PrivatBank");
+        Account acc1 = new Account("acc1");
+        Account acc2 = new Account("acc2");
 
+        acc1.makeDeposit(200);
+        acc2.makeDeposit(300);
+
+        bank.addAccount(acc1);
+        bank.addAccount(acc2);
+
+        bank.Transfer(acc2, acc1, 100);
+
+        System.out.println(acc1);
+        System.out.println(acc2);
     }
 }
